233622e43d4ec0202aa1b77055ad4c14ff20f6be
Matteo <matteomariotti0301@gmail.com>
2021-12-09 10:51:55 +0100

    Enabled cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 let Express = require("express");
 let bodyParser = require("body-parser");
 let MongoClient = require("mongodb").MongoClient;
+let cors = require("cors");
 
 let CONNECTION_STRING = "mongodb+srv://g11:$ciE!964Wg@dealsdetechtor.ybwtc.mongodb.net/DealsDeTECHtor?retryWrites=true&w=majority";
 let DATABASE = "DealsDeTECHtor";
@@ -9,6 +10,7 @@ let database;
 let app = Express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.listen(1234, () => {
     MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
